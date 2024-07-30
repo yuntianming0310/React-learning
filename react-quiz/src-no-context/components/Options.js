bf891599bba80bf27cca56b7bbebc77659a9261d
unknown <1677594629@qq.com>
2024-07-30 10:41:28 +0800

    react-quiz-context version

--- react-quiz/src-no-context/components/Options.js
+++ react-quiz/src-no-context/components/Options.js
@@ -1,7 +1,4 @@
-import { useQuiz } from "../contexts/QuizContext";
-
-function Options() {
-  const { question, dispatch, answer } = useQuiz();
+function Options({ question, dispatch, answer }) {
   const hasAnswered = answer !== null;
 
   return (
